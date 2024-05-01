dab6a110345f08f3e74447d75931ed6a76f935c6
metalpipemomo <bydn.mo@gmail.com>
2024-05-01 14:58:18 -0700

    Query object finished preliminarily, waiting on backend setup to test.

---
+++ apps/web/src/AxiosBase.ts
@@ -0,0 +1,49 @@
+import { useEffect } from 'react';
+import axios, { AxiosInstance } from 'axios';
+
+let axiosInstance: AxiosInstance | null = null;
+let requestInterceptorID: number | null = null;
+
+// A simple hook for creating or fetching an Axios instance
+export default function useAxios(): AxiosInstance | null {
+    useEffect(() => {
+        // Create Axios instance if not already created
+        if (!axiosInstance) {
+            axiosInstance = axios.create({
+                baseURL: 'http://our-url.com',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            // Add request interceptor if token is available
+            // Will change this method to cookies later on probably
+            const token = localStorage.getItem('jwtToken');
+            if (token) {
+                requestInterceptorID = axiosInstance.interceptors.request.use(
+                    (config) => {
+                        // Modify headers to include token
+                        const updatedConfig = { ...config };
+                        updatedConfig.headers.Authorization = `Bearer ${token}`;
+                        return updatedConfig;
+                    },
+                    (error) => {
+                        return Promise.reject(error);
+                    }
+                );
+            }
+
+            // Clean-up function
+            return () => {
+                // Remove request interceptor
+                if (axiosInstance && requestInterceptorID !== null) {
+                    axiosInstance.interceptors.request.eject(
+                        requestInterceptorID
+                    );
+                }
+            };
+        }
+    }, []);
+
+    return axiosInstance;
+}
