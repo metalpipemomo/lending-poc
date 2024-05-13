018143105ea92989db227d1ac8effc699a3caf0c
ZoravarLalli <lallizora@gmail.com>
2024-05-13 16:48:19 -0700

    pushing to merge main into branch and coninue

--- apps/web/app/components/OfferBox.tsx
+++ apps/web/app/components/OfferBox.tsx
@@ -1,6 +1,11 @@
-// Omitting the use client directive as we want to fetch and render dashboard offers on server not client.
+// If we want to optimize for server side then we will not use client directive as we want to fetch and render dashboard offers on server not client.
+// need to modify axios base to handle server side API calls if we do optimize this
+'use client'
 import React from 'react'
 import { ObjectId } from 'mongodb';
+// Since client rendered we will use the react state hooks
+import { useState, useEffect } from 'react';
+//import Axios from "../../lib/AxiosBase"; // for api calls
 
 // Define an interface for explicit TS type definition according to schema
 interface Offer{
@@ -18,11 +23,28 @@ interface Offer{
 }
 
 // React component for an offer loaded from the DB. Displayed on dashboard.
-const OfferBox: React.FC  = async () => { // explicit type on OfferBox is inferred for the prop
-  // TODO: Handle no offers loaded yet case and display warning
+const OfferBox: React.FC  = () => { // explicit type on OfferBox is inferred for the prop
+  const [offers, setOffers] = useState<Offer[]>([]);
+  //const api = Axios(); // Note have to do this outside of conditional code blocks and hooks -> HAVING ISSUES WITH REQUESTS NOT GOING OVER NETWORK USING AXIOS
 
-  const res = await fetch('/loan-service/offers/');
-  const offers: Offer[] = await res.json();
+  // Want to fetch and render in a useEffect hook
+  useEffect(()=>{
+    console.log("in useEffect on offer box")
+    const fetchOffers = () => {
+      console.log("attempting fetch offers")
+      api?.get('loan-service/offers/')
+        .then((res) => {
+          console.log('full response of offers: ', res);
+          console.log('Got offers res on front-end:', res.data);
+          setOffers(res.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching offers on front-end:', error.message);
+        });
+    }
+
+    fetchOffers();
+  }, []); // No dependency only running on load/mount
 
   return (
     <div id="dashboard-offers-container" className="mt-2 size-fit pl-1">
