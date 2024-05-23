cc6111f7c05517a65b32c0bbaaa2309b00a33a59
markomhcl <marko.miletic@hcl.com>
2024-05-23 01:17:04 -0700

    add kafka message queue production

--- apps/matching_service/src/util/findMatches.ts
+++ apps/matching_service/src/util/findMatches.ts
@@ -1,10 +1,19 @@
 import { LoanModel, Match, MatchModel } from '@repo/models'; // Import your models
+import { Kafka } from 'kafkajs'; // Import kafkajs
+
+// Initialize Kafka producer
+const kafka = new Kafka({
+  clientId: 'producer',
+  brokers: ['localhost:9093']
+});
+
+const producer = kafka.producer();
 
 export async function findMatches() {
+  await producer.connect();
   // Fetch all loan offers and borrowing requests
   const loanOffers = await LoanModel.find({ isLoan: "true" });
   const borrowRequests = await LoanModel.find({ isLoan: "false" });
-  
 
   const matches: Match[] = [];
 
@@ -19,19 +28,33 @@ export async function findMatches() {
         new Date(borrowRequest.dueDate) <= new Date(loanOffer.dueDate)
       ) {
         // Create a match
-        matches.push({
+        const match = {
           loanerId: loanOffer.userId,
           borrowerId: borrowRequest.userId,
           loanId: loanOffer._id,
           borrowId: borrowRequest._id,
           status: 'pending_match'
-        });
+        };
+        matches.push(match);
       }
     });
   });
   console.log('Matches to be inserted:', matches);
   await MatchModel.insertMany(matches);
   console.log(`${matches.length} matches inserted into the database.`);
+  
+  for (const match of matches) {
+    await producer.send({
+      topic: 'test-topic',
+      messages: [
+        { value: JSON.stringify(match) }
+      ]
+    });
+  }
+
+  console.log('All matches sent to Kafka broker.');
+
+  await producer.disconnect();
 }
 
 
