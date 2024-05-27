8688e535250531ac332a9ad4f9d439f9f271fbcc
markomhcl <marko.miletic@hcl.com>
2024-05-27 15:31:19 -0700

    run matching on execution and every 30 minutes

--- apps/matching_service/src/index.ts
+++ apps/matching_service/src/index.ts
@@ -1,31 +1,28 @@
 // Used from dotenv package to use env variables
 // MUST DEFINE MONGO_URI and PORT variables in a .env file in this server directory
-import "dotenv/config"
+import "dotenv/config";
 import mongoose from 'mongoose';
-import readline from 'readline';
 import { findMatches } from './util/findMatches';
+
+const intervalMinutes = 30;
+
+async function performMatching() {
+    try {
+        await findMatches();
+        console.log('Matching complete');
+    } catch (error) {
+        console.error('Error during matching:', error);
+    }
+    console.log(`Next matching occurring in ${intervalMinutes} minutes`);
+}
 
 // DB connection with connection string and use the options as second arg
 mongoose.connect(process.env.MONGO_URI!, { dbName: 'Loans' })
     .then(() => {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-
-        // Wait for user input to create matches
-        rl.question('Hit Enter to create matches...', async () => {
-            rl.close();
-            try {
-                await findMatches();
-                console.log('Matching complete');
-            } catch (error) {
-                console.error('Error during matching:', error);
-            } finally {
-                process.exit(0); // Exit the process
-            }
-        });
+        console.log("Connected to MongoDB");
+        performMatching();
+        setInterval(performMatching, intervalMinutes * 60 * 1000);
     })
-    .catch ((error) => {
-    console.log(error);
-});
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
