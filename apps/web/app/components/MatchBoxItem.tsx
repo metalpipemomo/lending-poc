17934747046292d7b9f63c8e465cf2a62318e11b
Zoravar Lalli <36012512+ZoravarLalli@users.noreply.github.com>
2024-05-27 18:25:40 -0700

    added some buttons for accept/reject need to define click handlers still

--- apps/web/app/components/MatchBoxItem.tsx
+++ apps/web/app/components/MatchBoxItem.tsx
@@ -1,6 +1,8 @@
 'use client'
 import React, {useState, useEffect} from 'react'
 import { ObjectId } from 'mongodb';
+import { ImCross } from "react-icons/im";
+import { ImCheckmark } from "react-icons/im";
 
 
 // Define an interface for explicit TS type definition according to schema of match
@@ -30,7 +32,7 @@ const MatchBoxItem = ({match} : {match: Match}) => {
   return (
     <li key={match._id.toString()} className="mb-2 w-full px-2 py-2 text-xs text-slate-700 bg-gray-50 rounded-lg relative">
       <div className="inline-block">
-        <h1 className="text-xl ">{isLender ? "Matched Borrower" : "Matched Lender"}</h1>
+        <h1 className="text-xl pb-1">{isLender ? "Matched Borrower" : "Matched Lender"}</h1>
         <span className="text-md"><span className="font-bold pr-0.5"> Lending user: </span>{match.loanerId} </span><br/>
         <span className="text-md"><span className="font-bold pr-0.5"> Borrowing user: </span>{match.borrowerId} </span>
         <h2 className="text-md mt-4"><span className="font-bold">Status: </span><span className={match.status == "pending_match" ? 'text-[#FF0000] text-xs pt-6' : 'text-[#00FF21] text-xs pt-6'}>{match.status == "pending_match" ? 'pending' : 'accepted'}</span></h2>
@@ -42,6 +44,10 @@ const MatchBoxItem = ({match} : {match: Match}) => {
         <div className="text-right pt-1"><span className={`${offer.riskLevel === "high-risk" ? 'text-[#FF0000]': offer.riskLevel === "low-risk" ? 'text-[#00FF21]': ''}`}>{offer.riskLevel}</span></div>
       </div> */}
       <span className="absolute right-2 bottom-1"><span className="font-bold">Offer ID: </span>{match._id.toString()}</span>
+      <div className="absolute top-2 right-2 flex justify-between gap-1.5 w-fit">
+        <span id="dashboard-link-view-matches" className="w-fit p-1.5 cursor-pointer font-semibold rounded-full shadow-sm border border-[#FF0000] bg-[#FF0000]  text-white hover:bg-gray-50 hover:text-[#FF0000] hover:border-[#FF0000] focus:outline-none focus:ring-2"><h2 className=""><ImCross /></h2></span>
+        <span id="dashboard-link-new-offer" className="w-fit p-1 cursor-pointer text-base font-semibold rounded-full shadow-sm border border-[#00FF21] bg-[#00FF21] text-white hover:bg-gray-50 hover:text-[#00FF21] hover:border-[#00FF21] focus:outline-none focus:ring-2"><h2 className=""><ImCheckmark /></h2></span>
+      </div>
     </li>
   )
 }
--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -13,8 +13,8 @@ const MainDashPage = () => {
         <div id="dashboard-header" className="flex justify-between w-full h-fit p-1.5 pl-3 text-slate-700 bg-white">
           <h1 className="text-2xl">Lending Service POC</h1>
           <div className="flex justify-between gap-1.5">
-            <Link href="/dashboard/matches" id="dashboard-link-view-matches" className="w-fit px-3 py-2 text-sm font-semibold rounded-full shadow-sm  bg-black text-white hover:bg-gray-300 hover:text-gray-800 hover:border-gray-800 focus:outline-none focus:ring-2"><h2 className="">View Matches</h2></Link>
-            <Link href="/dashboard/create" id="dashboard-link-new-offer" className="w-fit px-3 py-2 text-sm font-semibold rounded-full shadow-sm  bg-black text-white hover:bg-gray-300 hover:text-gray-800 hover:border-gray-800 focus:outline-none focus:ring-2"><h2 className="">Make a New Offer</h2></Link>
+            <Link href="/dashboard/matches" id="dashboard-link-view-matches" className="w-fit px-3 py-2 text-sm font-semibold rounded-full shadow-sm border border-black bg-black text-white hover:bg-gray-300 hover:text-gray-800 hover:border-gray-800 focus:outline-none focus:ring-2"><h2 className="">View Matches</h2></Link>
+            <Link href="/dashboard/create" id="dashboard-link-new-offer" className="w-fit px-3 py-2 text-sm font-semibold rounded-full shadow-sm border border-black bg-black text-white hover:bg-gray-300 hover:text-gray-800 hover:border-gray-800 focus:outline-none focus:ring-2"><h2 className="">Make a New Offer</h2></Link>
           </div>
         </div>
         <OfferBox/>
