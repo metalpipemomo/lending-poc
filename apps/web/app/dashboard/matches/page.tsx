884b8cb4334ad4d3a53f177aac05d4533f6c8b48
Zoravar Lalli <36012512+ZoravarLalli@users.noreply.github.com>
2024-05-28 16:03:11 -0700

    added header on registration page for consistency

--- apps/web/app/dashboard/matches/page.tsx
+++ apps/web/app/dashboard/matches/page.tsx
@@ -13,7 +13,7 @@ const ViewMatchesPage = () => {
       <div id="matches-container" className="">
         {/* TODO: The header used in all the separate pages should be an importable component as it is used everywhere */}
         <div id="matches-header" className="flex justify-between w-full h-fit p-1.5 pl-3 text-slate-700 bg-gray-50">
-          <h1 className="text-2xl">Lending Service POC</h1>
+          <Link href="/"><h1 className="text-2xl">Lending Service POC</h1></Link>
           {/* Nav button should also be an imported component used in the imported header component */}
           <Link href="/dashboard" id="matches-link-back" className="w-[142px] px-3 py-2 text-sm font-semibold rounded-full shadow-sm border border-black bg-black text-center text-white hover:bg-gray-300 hover:text-gray-800 hover:border-gray-800 focus:outline-none focus:ring-2"><h2 className="">Offer Dashboard</h2></Link>
         </div>
